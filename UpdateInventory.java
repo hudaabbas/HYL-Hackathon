1e77ecae8a06550d1bdef49b23618841980f024d
nmel88 <81481184+nmel88@users.noreply.github.com>
2021-03-27 15:36:27 -0600

    Update UpdateInventory.java

--- UpdateInventory.java
+++ UpdateInventory.java
@@ -9,66 +9,98 @@
 
 import java.sql.*;
 
-public class UpdateInventory {
-	public String[] itemsOrdered;
-	private Connection dbConnect;
+import java.sql.*;
+
+public class UpdateInventory extends DatabaseAccess {
 	
+	UpdateInventory(String DBURL, String USERNAME, String PASSWORD) {
+		super(DBURL, USERNAME, PASSWORD);
+	}
+
+
+	private String[] itemsOrdered;
+	
+	//Setter
 	public void setItemsOrdered(String[] items) {
 		this.itemsOrdered=items;
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//Getter
+	public String[] getItemsOrdered() {
+		return this.itemsOrdered;
 	}
 	
 
-	/**
-	 * Updates database and changes all categories for a selected item to N
+	/*
+	 * Deletes all the items in the list
 	 */
-	public void changeToN(String[] itemsOrdered, String table) {
-		String[] categories=null;
-		
-		//Gets all the column names 
+	public void removeItem(String table) {
 		try {
-			   Statement st= dbConnect.prepareStatement(table);
-			   ResultSet rs = st.executeQuery("SELECT * FROM "+table);
+			for(int i=0;i<itemsOrdered.length;i++) {
+			   String query= "DELETE FROM "+table+" WHERE ID = ?";
+			   PreparedStatement st= getDbConnect().prepareStatement(query);
 			   
-			   ResultSetMetaData rsmd = rs.getMetaData();
-			   int columnCount = rsmd.getColumnCount()-4;
-			   for (int i = 1,j=0; i <= columnCount; i++,j++ ) {
-				   if(!rsmd.getColumnName(i).equals("ID")&&!rsmd.getColumnName(i).equals("Type")
-					   &&!rsmd.getColumnName(i).equals("Price")&&!rsmd.getColumnName(i).equals("MaunID")){
-					  categories[j] = rsmd.getColumnName(i);
-					}
-			   }
-			   st.close();
-		   }
-		   catch(SQLException e) {
-			   System.out.println("Error, unable to get categories");
-		   }
-		
-		//Updates the table and sets all categories to N for items in the array
-		 try {
-			 for(int i=0;i<itemsOrdered.length;i++) {
-				 int j=0;
-				 while(j<categories.length) {
-			   String query= "UPDATE "+table+"SET ?='N' WHERE ID = ?";
-			   PreparedStatement st= dbConnect.prepareStatement(query);
-			   
-			   st.setString(1, categories[j]);
-			   st.setString(2, itemsOrdered[i]);
-			   
+			   st.setString(1, itemsOrdered[i]);
+			
 			   int rows=st.executeUpdate();
 			   System.out.println("Rows updated: "+rows);
-				 } 
-			 }
-			 
-			 //Should close it but the IDE is being picky about brackets so add it in later
-			//  st.close();
-		   } 
+			   
+			   st.close();
+			}
+		   }
 		   catch(SQLException e) {
-			   System.out.println("Error, unable to update item status");
+			   System.out.println("Error, unable to delete teacher");
 		   }
 	}
-
 }
+
+// 	/**
+// 	 * Updates database and changes all categories for a selected item to N
+// 	 */
+// 	public void changeToN(String[] itemsOrdered, String table) {
+// 		String[] categories=null;
+		
+// 		//Gets all the column names 
+// 		try {
+// 			   Statement st= dbConnect.prepareStatement(table);
+// 			   ResultSet rs = st.executeQuery("SELECT * FROM "+table);
+			   
+// 			   ResultSetMetaData rsmd = rs.getMetaData();
+// 			   int columnCount = rsmd.getColumnCount()-4;
+// 			   for (int i = 1,j=0; i <= columnCount; i++,j++ ) {
+// 				   if(!rsmd.getColumnName(i).equals("ID")&&!rsmd.getColumnName(i).equals("Type")
+// 					   &&!rsmd.getColumnName(i).equals("Price")&&!rsmd.getColumnName(i).equals("MaunID")){
+// 					  categories[j] = rsmd.getColumnName(i);
+// 					}
+// 			   }
+// 			   st.close();
+// 		   }
+// 		   catch(SQLException e) {
+// 			   System.out.println("Error, unable to get categories");
+// 		   }
+		
+// 		//Updates the table and sets all categories to N for items in the array
+// 		 try {
+// 			 for(int i=0;i<itemsOrdered.length;i++) {
+// 				 int j=0;
+// 				 while(j<categories.length) {
+// 			   String query= "UPDATE "+table+"SET ?='N' WHERE ID = ?";
+// 			   PreparedStatement st= dbConnect.prepareStatement(query);
+			   
+// 			   st.setString(1, categories[j]);
+// 			   st.setString(2, itemsOrdered[i]);
+			   
+// 			   int rows=st.executeUpdate();
+// 			   System.out.println("Rows updated: "+rows);
+// 				 } 
+// 			 }
+			 
+// 			 //Should close it but the IDE is being picky about brackets so add it in later
+// 			//  st.close();
+// 		   } 
+// 		   catch(SQLException e) {
+// 			   System.out.println("Error, unable to update item status");
+// 		   }
+// 	}
+
+// }
